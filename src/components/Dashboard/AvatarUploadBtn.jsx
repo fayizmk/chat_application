2d8ad76aeed372e1e0f67e7423b9ee8cbcc47fa2
fayizmk <fayizmk20@gmail.com>
2021-12-12 16:06:11 +0530

    displayed user avatar

--- src/components/Dashboard/AvatarUploadBtn.jsx
+++ src/components/Dashboard/AvatarUploadBtn.jsx
@@ -4,6 +4,7 @@ import { Alert, Button, Modal } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { useModalState } from '../../misc/custom-hooks';
 import { database, storage } from '../../misc/firebase';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputType = '.png, .jpeg, .jpg, .webp';
 
@@ -76,6 +77,11 @@ const AvatarUploadBtn = () => {
 
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar
+        src={profile.avatar}
+        name={profile.name}
+        className="width-200 height-200 img-fullsize font-huge"
+      />
       <div>
         <label
           htmlFor="avatar-upload"
